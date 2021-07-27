267fb1f507c4127d7a6ac9290a64b786dedbb33f
ivanBalev <43599235+ivanBalev@users.noreply.github.com>
2021-07-28 01:18:41 +0300

    Finished optimizing index page video style and loading time.

---
+++ Web/BDInSelfLove.Web/wwwroot/Custom/js/homeVideoTitleLengthAdjust.js
@@ -0,0 +1,14 @@
+﻿document.querySelectorAll('.video-title').forEach(t => {
+    let innerText = t.innerText;
+    let windowWidth = window.innerWidth;
+
+    if (windowWidth < '768' && innerText.length > 24) {
+        t.innerText = innerText.substr(0, 24) + '...';
+    } else if (windowWidth <= '992' && innerText.length > 40) {
+        t.innerText = innerText.substr(0, 40) + '...';
+    } else if (windowWidth <= '1200' && innerText.length > 55) {
+        t.innerText = innerText.substr(0, 55) + '...';
+    } else if (innerText.length > 65) {
+        t.innerText = innerText.substr(0, 65) + '...';
+    }
+})
